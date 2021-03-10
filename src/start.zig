409f0bb1d794842e08188529769e31d6d3e64f35
Timon Kruiper <timonkruiper@gmail.com>
2021-03-10 15:08:35 +0100

    clear bss and copy data to ram on init
    This makes them actually work!

--- src/start.zig
+++ src/start.zig
@@ -9,8 +9,58 @@ pub export fn _start() callconv(.Naked) noreturn {
     // are added.
     _ = vectors;
 
+    copy_data_to_ram();
+    clear_bss();
+
     main.main();
     while (true) {}
+}
+
+fn copy_data_to_ram() void {
+    asm volatile (
+        \\  ; load Z register with the address of the data in flash
+        \\  ldi r30, lo8(__data_load_start)
+        \\  ldi r31, hi8(__data_load_start)
+        \\  ; load X register with address of the data in ram
+        \\  ldi r26, lo8(__data_start)
+        \\  ldi r27, hi8(__data_start)
+        \\  ; load address of end of the data in ram
+        \\  ldi r24, lo8(__data_end)
+        \\  ldi r25, hi8(__data_end)
+        \\  rjmp .L2
+        \\
+        \\.L1:
+        \\  lpm r18, Z+ ; copy from Z into r18 and increment Z
+        \\  st X+, r18  ; store r18 at location X and increment X
+        \\
+        \\.L2:
+        \\  cp r26, r24
+        \\  cpc r27, r25 ; check and branch if we are at the end of data
+        \\  brne .L1
+    );
+    // Probably a good idea to add clobbers here, but compiler doesn't seem to care
+}
+
+fn clear_bss() void {
+    asm volatile (
+        \\  ; load X register with the beginning of bss section
+        \\  ldi r26, lo8(__bss_start)
+        \\  ldi r27, hi8(__bss_start)
+        \\  ; load end of the bss in registers
+        \\  ldi r24, lo8(__bss_end)
+        \\  ldi r25, hi8(__bss_end)
+        \\  ldi r18, 0x00
+        \\  rjmp .L4
+        \\
+        \\.L3:
+        \\  st X+, r18
+        \\
+        \\.L4:
+        \\  cp r26, r24
+        \\  cpc r27, r25 ; check and branch if we are at the end of bss
+        \\  brne .L3
+    );
+    // Probably a good idea to add clobbers here, but compiler doesn't seem to care
 }
 
 pub fn panic(msg: []const u8, error_return_trace: ?*@import("builtin").StackTrace) noreturn {
